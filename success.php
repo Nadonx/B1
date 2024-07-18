3a96dcfee1a204271b4842c4efca23477c14ddb3
Shaw <139181019+Shawliiath@users.noreply.github.com>
2024-07-18 16:15:58 +0200

    Update success.php

--- success.php
+++ success.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['username'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienvenue</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="index2.css">
     <script>
         function toggleMenu() {
             var nav = document.getElementById('side-nav');
